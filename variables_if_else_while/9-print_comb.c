16a51c553b9f1e56070dd1b06ba93a3700f6c66b
Vadim G. <gavet.vadim@gmail.com>
2025-10-21 14:08:57 +0200

    Fixed file 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -11,7 +15,7 @@ int main(void)
 			putchar(n);
 			putchar(',');
 			putchar(' ');
-       		}
+		}
 		putchar(n);
 	}
 	return (0);
