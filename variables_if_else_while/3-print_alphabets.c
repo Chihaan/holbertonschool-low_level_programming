36e6ad15ae23f81dc65596ddef67f4fb85881bd0
Vadim G. <gavet.vadim@gmail.com>
2025-10-21 09:54:55 +0200

    Fixed comments in 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Déclaration de c et C type char
+ * Boucle for 1 : affiche l'alphabet en minuscule
+ * Boucle for 2 : affiche l'alphabet en majuscule
+ * putchar de newline
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char c;
 	char C;
 
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
-	for(C = 'A'; C <= 'Z'; C++)
+	for (C = 'A'; C <= 'Z'; C++)
 	{
 		putchar(C);
 	}
