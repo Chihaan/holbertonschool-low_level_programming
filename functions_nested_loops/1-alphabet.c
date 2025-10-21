d5552457e0a8f48b1336659a6a9ec0a3ae69784f
Vadim G. <gavet.vadim@gmail.com>
2025-10-21 19:54:33 +0200

    Added file 1 project 3

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.c"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; a <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('/n');
+	return (0);
+}
