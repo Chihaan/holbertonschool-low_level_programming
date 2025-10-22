a52b37f53fcd52f409fe8ca76938dfcc097add8f
Vadim G. <gavet.vadim@gmail.com>
2025-10-22 18:27:57 +0200

    added func 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * times_table - check the code
+ * @void :
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i * j <= 9)
+			{
+				_putchar(i * j + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(i * j / 10 + '0');
+				_putchar(i * j % 10 + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
