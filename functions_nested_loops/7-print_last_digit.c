a3efc1449a7b3f283b9fb443e7555675ac431f90
Vadim G. <gavet.vadim@gmail.com>
2025-10-22 10:59:42 +0200

    added func 7 project 3

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - used to print the last digit of a number
+ * @d : the number tested
+ * Return: the value of 10 % 10.
+ */
+int print_last_digit(int d)
+{
+	return (d % 10);
+}
