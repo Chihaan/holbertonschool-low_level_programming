611ff00977f3394ae18ea5a4e782dc4ebeabf824
Vadim G. <gavet.vadim@gmail.com>
2025-10-22 13:29:06 +0200

    try fix func 7 last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,16 @@
 /**
  * print_last_digit - used to print the last digit of a number
  * @d : the number tested
- * @b : is d % 10
- * Return: the value of 10 % 10.
+ * Return: le dernier chiffre de @d.
  */
 int print_last_digit(int d)
 {
 	int b = d % 10;
+
+	if (b < 0)
+	{
+		b = -b;
+	}
 	_putchar(b + '0');
 	return (b);
 }
