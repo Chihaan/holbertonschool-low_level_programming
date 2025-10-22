778261db512c866f16f17035332659c4f4bd4727
Vadim G. <gavet.vadim@gmail.com>
2025-10-22 16:31:50 +0200

    added func 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - check the code
+ * @a : first number to add
+ * @b : second numer tu add
+ * Return: sum of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
