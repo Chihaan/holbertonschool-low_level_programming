aa4f8010b2e60e09d44bd97554145a0cf6ebc16c
Vadim G. <gavet.vadim@gmail.com>
2025-10-24 11:52:39 +0200

    Test file 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,18 @@
-#include <stdio.h>
-
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 for stderr.
+ */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(
+		2, "and that piece of art is useful\""
+		" - Dora Korpar, 2015-10-19\n",
+		strlen("and that piece of art is useful\""
+		       " - Dora Korpar, 2015-10-19\n")
 
+			);
 	return (1);
 }
