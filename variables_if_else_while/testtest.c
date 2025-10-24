db5762fa86831e64d1a886ccc629d9b25ff09822
Vadim G. <gavet.vadim@gmail.com>
2025-10-24 15:56:15 +0200

    Added file 100

---
+++ variables_if_else_while/testtest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+        char i = 'a';
+
+        for (i = 'a'; i <= 'z'; i++)
+	{
+                putchar(i);
+	}
+        putchar('\n');
+        return (0);
+}
