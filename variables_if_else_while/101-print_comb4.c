19e9d05a678b009ddba90cfd662f28e2a87cd03f
Vadim G. <gavet.vadim@gmail.com>
2025-10-26 14:51:53 +0100

    fixed advanced 101

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -10,15 +10,15 @@ int main(void)
 
 	for (i = 0; i <= 7; i++)
 	{
-		for(j = 0; j <= 8; j++)
+		for (j = 0; j <= 8; j++)
 		{
-			for(k = 0; k <=9; k++)
+			for (k = 0; k <= 9; k++)
 			{
 				if (i < j && j < k)
 				{
 					putchar(i + '0');
-                                        putchar(j + '0');
-                                        putchar(k + '0');
+					putchar(j + '0');
+					putchar(k + '0');
 					if (i != 7)
 					{
 						putchar(',');
