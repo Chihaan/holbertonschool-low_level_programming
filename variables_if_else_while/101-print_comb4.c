a54679d57c735d8697a6d530f2a29633352b5161
Vadim G. <gavet.vadim@gmail.com>
2025-10-26 14:39:10 +0100

    File 101 advanced

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @int - variables pour boucle 1 et 2 et 3
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for(j = 0; j <= 8; j++)
+		{
+			for(k = 0; k <=9; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
