abf836c5306c29e06b9be141611b2cae0c64dfe2
Vadim G. <gavet.vadim@gmail.com>
2025-10-27 09:08:31 +0100

    Added file O

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,15 @@
 #include "main.h"
+/**
+ * _isupper - check if its lower case
+ * @c: The character to check
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
