2d57de7908e729792052fff198e778b795d154dd
Vadim G. <gavet.vadim@gmail.com>
2025-10-27 23:00:58 +0100

    Added file 2 of project 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,15 @@
 #include "main.h"
 /**
- * reset_to_98 - check the code
- * 
+ * _strlen - retourne la longueur d'une chaîne de caractères
+ * @s: la chaîne dont on veut connaître la longueur
+ *
+ * Return: la longueur de la chaîne (nombre de caractères avant '\0')
  */
 int _strlen(char *s)
 {
-    
+	int len = 0;
+
+	while (s[len] != '\0')
+	len++;
+return (len);
 }
