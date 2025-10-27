f24d6acb93bd3e55472718d1d8433e20461faefa
Vadim G. <gavet.vadim@gmail.com>
2025-10-27 09:22:57 +0100

    Added fix for betty file 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints all numbers from o to 9
+ *
+ * @void : no return
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
