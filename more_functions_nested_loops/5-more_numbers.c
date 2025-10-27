65a28b879a8005520ff9dfde476146894c204e89
Vadim G. <gavet.vadim@gmail.com>
2025-10-27 09:45:40 +0100

    Added fix file 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,13 @@ void more_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j < 10)
+				_putchar(j + '0');
+			else
+				_putchar(j % 10 '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
