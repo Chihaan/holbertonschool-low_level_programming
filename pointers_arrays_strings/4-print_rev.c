c4ef1ef9bcb32724e48d7eaba5662ff266222f71
Vadim G. <gavet.vadim@gmail.com>
2025-10-27 23:39:14 +0100

    Added file 4 of project 2

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,24 @@
 #include "main.h"
 /**
- * reset_to_98 - check the code
- * 
+ * print_rev - affiche une chaîne de caractères en ordre inverse
+ * @s: la chaîne à afficher
+ *
+ * Return: rien (void)
  */
 void print_rev(char *s)
 {
-    
+	int len = 0;
+	int end;
+
+	while (s[len] != '\0')
+		len++;
+
+	end = len - 1;
+
+	while (end >= 0)
+	{
+		_putchar(s[end]);
+		end--;
+	}
+	_putchar('\n');
 }
