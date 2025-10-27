89045b88674eba398b993b676a04194975c36a29
Vadim G. <gavet.vadim@gmail.com>
2025-10-27 01:44:10 +0100

    Fix suaqre ?

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,13 +8,14 @@ void print_square(int size)
 {
 	int i, j;
 
+	if (size <= 0)
+		putchar('\n');
 	for (i = 1; i <= size; i++)
 	{
 		for (j = 1; j <= size; j++)
 		{
 			_putchar('#');
 		}
-		_putchar('\n');
 	}
 	_putchar('\n');
 }
