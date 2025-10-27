cc0f9c972f414ba07172745a43b1693cd189d8aa
Vadim G. <gavet.vadim@gmail.com>
2025-10-27 23:10:38 +0100

    Added file 3 of project 2

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,18 @@
 #include "main.h"
 /**
- * reset_to_98 - check the code
- * 
+ * _puts - affiche une chaîne de caractères suivie d'une nouvelle ligne
+ * @str: la chaîne à afficher
+ *
+ * Return: rien (void)
  */
 void _puts(char *str)
 {
-    
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
