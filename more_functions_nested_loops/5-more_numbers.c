f692dff9294dbbd3f550e1bc56e0b6280e04ef02
Vadim G. <gavet.vadim@gmail.com>
2025-10-27 09:30:48 +0100

    Added file 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - write numbers x10
+ * @void: No return
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(j + '0');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
