17bf40b9441cd0aaf65e5476827fc3c54b2aa38c
Vadim G. <gavet.vadim@gmail.com>
2025-10-27 09:16:06 +0100

    Added file 2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * mul - multiple two caracteres
- * @a,b : The character to use
- *
+ * @a : The character to use
+ * @b : The charactert to use
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int mul(int a, int b)
 {
-    int c = a * b;
-    return c;
+	int c = a * b;
+	return (c);
 }
