6341423ecd800086a635c4cbd9babb064550c540
Vadim G. <gavet.vadim@gmail.com>
2025-10-28 10:25:03 +0100

    Added file 6 project 2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,20 @@
 #include "main.h"
 /**
- * reset_to_98 - check the code
- * 
+ * puts2 - affiche un caractère sur deux d'une chaîne de caractères
+ * @str: pointeur vers la chaîne à afficher
+ *
+ * Cette fonction parcourt la chaîne pointée par @str et affiche
+ * uniquement un caractère sur deux, en commençant par le premier.
+ * À la fin, elle ajoute un saut de ligne ('\n').
  */
 void puts2(char *str)
 {
+	int i = 0;
 
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
 }
