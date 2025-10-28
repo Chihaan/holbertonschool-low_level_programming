c15d1eafc95fa87e28dad2446aa04951789fbb19
Vadim G. <gavet.vadim@gmail.com>
2025-10-28 13:53:32 +0100

    Added file 8 project 2

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * reset_to_98 - check the code
- * 
+ * print_array - affiche les éléments d’un tableau d’entiers
+ * @a: pointeur vers le premier élément du tableau
+ * @n: nombre d’éléments à afficher
+ *
+ * Cette fonction affiche les n premiers
+ * éléments du tableau d’entiers pointé
+ * par @a. Les éléments sont séparés par
+ * une virgule et un espace, puis un
+ * saut de ligne est ajouté à la fin de l’affichage.
  */
 void print_array(int *a, int n)
 {
-    
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
 }
