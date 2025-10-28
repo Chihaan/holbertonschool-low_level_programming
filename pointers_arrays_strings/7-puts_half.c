37e57f1f06004c2f03b64961ae75e8ad6d6bcb14
Vadim G. <gavet.vadim@gmail.com>
2025-10-28 12:35:12 +0100

    Try fix in file 7 project 2

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,22 +13,15 @@ void puts_half(char *str)
 	}
 	start = len / 2;
 
-	if ((len + 1) % 2 == 0)
-	{
-		start ++;
-		while (str[start] != '\0')
-		{
-			_putchar(str[start]);
-			start++;			
-		}
-	}
+	if (len % 2 == 0)
+		start = len / 2;
 	else 
+		start = (len - 1) / 2 + 1;		
+
+	while (str[start] != '\0')
 	{
-		while (str[start] != '\0')
-		{
-			_putchar(str[start]);
-			start++;		
-		}
-	}	
+		_putchar(str[start]);
+		start++;			
+	}
 	_putchar('\n');
 }
