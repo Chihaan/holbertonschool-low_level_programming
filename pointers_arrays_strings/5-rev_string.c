fab82755f6529d1cd16bb950787c04ed6dfb012c
Vadim G. <gavet.vadim@gmail.com>
2025-10-28 09:45:52 +0100

    Added file 5 of project 2

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,26 @@
 #include "main.h"
 /**
- * reset_to_98 - check the code
- * 
+ * rev_string - inverse une chaîne de caractères
+ * @s: pointeur vers la chaîne à inverser
+ *
+ * Cette fonction calcule la longueur de la chaîne, puis échange les
+ * caractères du début et de la fin jusqu’à ce que toute la chaîne soit inversée.
  */
 void rev_string(char *s)
 {
+	int start = 0, end, len = 0, tmp;
+
+	while (s[len] != '\0')
+		len++;
 	
+	end = len - 1;
+
+	while (start < end)
+	{
+		tmp = s[start];
+		s[start] = s[end];
+		s[end] = tmp;
+		start++;
+		end--;
+	}
 }
