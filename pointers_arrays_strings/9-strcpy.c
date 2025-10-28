728b5c90094d6ee2b7ea9fffe33f93daa63eec60
Vadim G. <gavet.vadim@gmail.com>
2025-10-28 15:23:27 +0100

    Added file 9 project 2

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,25 @@
 #include "main.h"
 /**
- * reset_to_98 - check the code
- * 
+ * _strcpy - copie une chaîne de caractères dans une autre
+ * @dest: pointeur vers le buffer de destination
+ * @src: pointeur vers la chaîne source à copier
+ *
+ * Cette fonction copie la chaîne pointée par @src, y compris le caractère
+ * nul de fin ('\0'), dans le buffer pointé par @dest.
+ * Return: un pointeur vers la chaîne de destination.
  */
 char *_strcpy(char *dest, char *src)
 {
-    
+	char *ogdest = *dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		*src++;
+		*dest++;
+	}
+	*dest = '\0';
+
+	return (*ogdest);
+
 }
