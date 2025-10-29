fbd5b690b09d5d1ca49fcbaa4efda4f64332b0ff
Vadim G. <gavet.vadim@gmail.com>
2025-10-30 00:09:36 +0100

    Fix in file 6 project 3

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,36 @@
 #include "main.h"
-
+/**
+ * *cap_string - Converts all lowercase letters in a string to uppercase.
+ * @str: Pointer to the string to be modified.
+ * This function iterates through each character of the string pointed to by
+ * str, converting any lowercase letter to its uppercase equivalent. The
+ * function modifies the string in place and returns the modified string.
+ *
+ * Return: Pointer to the converted string.
+ */
 char *cap_string(char *str)
 {
-	char *ptr = str;
+	char *ptr = str, sprt[13] = {',', ';', '.', '!', '?',
+		 '"', '(', ')', '{', '}', '\n', ' ', '\t'};
+	int i;
+
+	if (*ptr >= 'a' && *ptr <= 'z')
+		*ptr = *ptr - 32;
 
 	while (*ptr != '\0')
-		if ((*ptr - 1 <= '0' && *ptr - 1 >='9') && (*ptr - 1 <= 'a' && *ptr - 1 >= 'z'))
-			*ptr = *ptr - 32;
+	{
+		for (i = 0; i < 13; i++)
+		{
+			if (*ptr == sprt[i])
+			{
+				if (*(ptr + 1) >= 'a' && *(ptr + 1) <= 'z')
+				{
+					ptr = ptr + 1;
+					*ptr = *ptr - 32;
+				}
+			}
+		}	
 		ptr++;
+	}
 	return (str);
 }
