e78cfce9186f82d4f7a95fa9d95a2fe9921d20a4
Vadim G. <gavet.vadim@gmail.com>
2025-10-29 21:21:08 +0100

    Fix file 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int cmp = 0;
-
 		while (*s1 == *s2)
 		{
 			s1++;
 			s2++;
 		}
-		
 	return (*s1 - *s2);
 }
--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-	
+	char *ptr = str;
+
+	while (*ptr != '\0')
+		if ((*ptr - 1 <= '0' && *ptr - 1 >='9') && (*ptr - 1 <= 'a' && *ptr - 1 >= 'z'))
+			*ptr = *ptr - 32;
+		ptr++;
+	return (str);
 }
