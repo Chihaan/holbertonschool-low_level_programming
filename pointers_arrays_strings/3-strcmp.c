d07bcaa54cd464bb207a57122d9e8f55a5c585da
Vadim G. <gavet.vadim@gmail.com>
2025-10-29 14:07:36 +0100

    Added file 3 of project 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,25 @@
 #include "main.h"
-
+/**
+ * _strcmp - Compare deux chaînes de caractères.
+ * @s1: Première chaîne à comparer.
+ * @s2: Deuxième chaîne à comparer.
+ *
+ * Return:
+ *   0  si les deux chaînes sont identiques,
+ *   une valeur négative si s1 < s2,
+ *   une valeur positive si s1 > s2 (selon l’ordre ASCII).
+ */
 int _strcmp(char *s1, char *s2)
 {
-	
+	int cmp = 0;
+
+	if (*s1 == *s2)
+	{
+		while (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+	}
+	return (*s1 - *s2);
 }
