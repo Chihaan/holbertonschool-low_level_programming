7fdd86b69ed4c55f2da36c2ba413f0563fa3cb44
Vadim G. <gavet.vadim@gmail.com>
2025-10-29 09:30:49 +0100

    Added file 1 of project 2

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,27 @@
 #include "main.h"
-
+/**
+ * _strncat - Concatène au plus n caractères
+ * de src à la fin de dest.
+ * @dest: Chaîne de destination.
+ * @src: Chaîne source.
+ * @n: Nombre max de caractères à copier.
+ *
+ * Return: Pointeur vers dest.
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	
+	char *ptr = dest;
+	int i;
+
+	while (*ptr != '\0')
+		ptr++;
+
+	for (int i = 0; i < n && *src != '\0; i++')
+	{
+		*ptr = *src;
+		src++;
+		ptr++;
+	}
+	*ptr = '\0';
+	return (dest);
 }
