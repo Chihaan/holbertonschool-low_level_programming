f1e2da9eef2ea26bd4eed5a7a6602e0517fcb47f
Vadim G. <gavet.vadim@gmail.com>
2025-10-29 11:48:22 +0100

    Added file 2 of project 3

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,30 @@
 #include "main.h"
-
+/**
+ * _strncpy - Copy at most n chars from src to dest
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Max number of chars to copy
+ *
+ * Return: Pointer to dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-	
+	int i;
+	char *odest = dest;
+
+	for (i = 0; i < n; i++)
+	{
+		if (*src != '\0')
+		{
+			*dest = *src;
+			dest++;
+			src++;
+		}
+		else
+		{
+			*dest = '\0';
+			dest++;
+		}
+	}
+	return (odest);
 }
