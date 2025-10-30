a583bd891ae3bac825cb3be2b9ee792579d749f8
Vadim G. <gavet.vadim@gmail.com>
2025-10-30 16:23:11 +0100

    File 10 in project 2

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -5,5 +5,21 @@
  */
 int _atoi(char *s)
 {
-    
+    int r = 0, n = 1;
+    char *os = s;
+
+    while (*os != '\0')
+    {
+	if (*s > '0' && *s < 9)
+	{
+		r = r * 10;
+		r = r + *s;
+	}
+	else if (*s = '-')
+		n = n * -1;
+	else if (*(s + 1) < '0' && *(s + 1) > ' 9')
+		break;
+	os++;
+    }
+    return (r * n);	
 }
