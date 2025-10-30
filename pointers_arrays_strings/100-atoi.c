c046dd7102f90a282d19eb183179980c6c9ed1f9
Vadim G. <gavet.vadim@gmail.com>
2025-10-30 20:12:22 +0100

    Fix file 10 in project 2

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,25 +1,26 @@
 #include "main.h"
 /**
- * reset_to_98 - check the code
- * 
+ * int _atoi - check the code
+ * @*s: 
+ * return: sign + result
  */
 int _atoi(char *s)
 {
-    int r = 0, n = 1;
-    char *os = s;
+	int r = 0, n = 1;
+	char *os = s;
 
-    while (*os != '\0')
-    {
-	if (*s > '0' && *s < 9)
+	while (*os != '\0')
 	{
-		r = r * 10;
-		r = r + *s;
+		if (*os >= '0' && *os <= '9')
+		{
+			r = r * 10;
+			r = r + *os - 48;
+		}
+		else if (*os == '-')
+			n = n * -1;
+		else if (r != 0)
+			break;
+		os++;
 	}
-	else if (*s = '-')
-		n = n * -1;
-	else if (*(s + 1) < '0' && *(s + 1) > ' 9')
-		break;
-	os++;
-    }
-    return (r * n);	
+	return (r * n);
 }
