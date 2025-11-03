73b720c7a7de048cdfc14b77305dbb6598eb84f1
Vadim G. <gavet.vadim@gmail.com>
2025-11-03 14:26:14 +0100

    Added file 4 of project 1

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,27 @@
 #include "main.h"
-
+/**
+ * _strpbrk - cherche le premier caractère de s présent dans accept
+ * @s: chaîne à parcourir
+ * @accept: ensemble de caractères à comparer
+ * Return: pointeur vers le premier caractère trouvé, ou NULL si aucun
+ */
 char *_strpbrk(char *s, char *accept)
 {
-	
+	char *ps = s, *paccept = accept;
+
+	while (*ps != '\0')
+	{
+		while (*paccept != '\0')
+		{
+			if (*ps == *paccept)
+			{
+				return (ps);
+			}
+			paccept++;
+		}
+		paccept = accept;
+		ps++;
+	}
+	return (0);
+
 }
