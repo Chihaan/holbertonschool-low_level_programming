5e88200489dc32be2885f45fd7ec3d350125f0d5
Vadim G. <gavet.vadim@gmail.com>
2025-11-03 21:19:53 +0100

    Added file 1 project 2

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - affiche une chaîne de caractères
+ * @s: pointeur vers la chaîne à afficher
+ * Return: rien
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
