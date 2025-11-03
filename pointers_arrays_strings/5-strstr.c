9a141abeee0ce56ae493d6c577e28c9f206b7e4d
Vadim G. <gavet.vadim@gmail.com>
2025-11-03 16:50:19 +0100

    Added file 5 of project 1

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,31 @@
 #include "main.h"
-
+/**
+ * _strstr - cherche la première occurrence de needle dans haystack
+ * @haystack: chaîne principale
+ * @needle: sous-chaîne à trouver
+ * Return: pointeur vers le début de la sous-chaîne trouvée
+ * , ou NULL si absente
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	
+	char *pneedle = needle, *phaystack = haystack;
+
+	if (*pneedle == '\0')
+		return (haystack);
+
+	while (*phaystack != '\0')
+	{
+		char *tph = phaystack, *start = phaystack;
+
+		while (*tph == *pneedle && *pneedle != '\0')
+		{
+			pneedle++;
+			tph++;
+		}
+		if (*pneedle == '\0')
+			return (start);
+
+		phaystack++;
+	}
+	return (0);
 }
