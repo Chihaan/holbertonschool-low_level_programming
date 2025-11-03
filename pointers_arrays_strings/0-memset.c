da8fed4c2ff11d8a675534fafa381174c645d57c
Vadim G. <gavet.vadim@gmail.com>
2025-11-03 09:52:24 +0100

    Fixed file 0 project 1

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * _memset - remplit une zone mémoire avec un octet donné
+ * @s: pointeur vers la zone mémoire à remplir
+ * @b: octet utilisé pour le remplissage
+ * @n: nombre d'octets à remplir
+ *
+ * Return: pointeur vers la zone mémoire s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
