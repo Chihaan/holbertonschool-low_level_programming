c92fd6605905cbccc39c9ecb6bb303b4b29a8b57
Vadim G. <gavet.vadim@gmail.com>
2025-11-03 10:19:54 +0100

    Added file 2 project 1

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,22 @@
 #include "main.h"
-
+/**
+ * _strchr - cherche un caractère dans une chaîne
+ * @s: chaîne où chercher
+ * @c: caractère à trouver
+ *
+ * Return: pointeur vers la première occurrence ou NULL
+ */
 char *_strchr(char *s, char c)
 {
-	
+	char *sptr = s;
+
+	while (*sptr != '\0')
+	{
+		if (*sptr == c)
+		{
+			return (sptr);
+		}
+		sptr++;
+	}
+	return (0);
 }
