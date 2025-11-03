9b4e0446cb9e9e366f7f2e46f859b846517e254a
Vadim G. <gavet.vadim@gmail.com>
2025-11-03 20:58:59 +0100

    Added file 8 project 1

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,8 +1,19 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_diagsums - affiche la somme des deux diagonales d’un carré
+ * @a: pointeur vers le tableau 2D (traité comme un tableau 1D)
+ * @size: taille du carré (nombre de lignes/colonnes)
+ * Return: rien
+ */
 void print_diagsums(int *a, int size)
 {
-	int i, sumg, sumd;
-	
+	int i, sumg = 0, sumd = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sumg += a[size * i + i];
+		sumd += a[size * i + (size - 1 - i)];
+	}
+	printf("%d, %d\n", sumg, sumd);
 }
