ec1a2f86326abf30f54a261ee80114d7cb607010
Vadim G. <gavet.vadim@gmail.com>
2025-11-03 22:34:02 +0100

    Added file 4 of project 2

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,6 +1,16 @@
 #include "main.h"
-
+/**
+ * _pow_recursion - calcule x à la puissance y de façon récursive
+ * @x: base
+ * @y: exposant
+ * Return: le résultat de x^y, ou -1 si y est négatif
+ */
 int _pow_recursion(int x, int y)
 {
-
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
