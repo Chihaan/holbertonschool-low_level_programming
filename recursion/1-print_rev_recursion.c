db4bd54605b24739b979282a5a19c1f3dec41923
Vadim G. <gavet.vadim@gmail.com>
2025-11-03 22:21:29 +0100

    Added file 3 of project 2

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -2,5 +2,14 @@
 
 void _print_rev_recursion(char *s)
 {
+	char *start = s;
+	int i;
 
+	if (*s != '\0')
+		_print_rev_recursion(s + 1);
+
+	for (i = *s; i >= *start; i--)
+	{
+		_putchar(i);
+	}
 }
--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,6 +1,15 @@
 #include "main.h"
-
+/**
+ * factorial - calcule la factorielle d’un nombre
+ * @n: entier dont on veut la factorielle
+ * Return: la factorielle de n, -1 si n est négatif
+ */
 int factorial(int n)
 {
-
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1));
 }
