7662de3dde9a96c4f9a6cb980d4ff98ad5a8dcb2
Vadim G. <gavet.vadim@gmail.com>
2025-11-04 15:26:06 +0100

    Added files projet 2

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _sqrt_helper - trouve la racine carrée naturelle d'un nombre.
+ * _sqrt_check - trouve la racine carrée naturelle d'un nombre.
  * @n: le nombre dont on cherche la racine.
  * @i: tentative actuelle.
  * Return: la racine carrée naturelle ou -1 si elle n'existe pas.
@@ -9,7 +9,7 @@ int _sqrt_check(int n, int i)
 {
 	if (i * i == n)
 		return (i);
-	if (i * i < 0)
+	if (i * i > n)
 		return (-1);
 	return (_sqrt_check(n, i + 1));
 }
--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,30 @@
 #include "main.h"
-
+/**
+ * _sqrt_check - trouve la racine carrée naturelle d'un nombre.
+ * @n: le nombre dont on cherche la racine.
+ * @i: tentative actuelle.
+ * Return: la racine carrée naturelle ou -1 si elle n'existe pas.
+ */
+int _is_prime(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (_is_prime(n, i + 2));
+}
+/**
+ * is_prime_number - dit si un entier est un nombre premier
+ * @n: entier à tester
+ * Return: 1 si n est premier, 0 sinon
+ */
 int is_prime_number(int n)
 {
-
+	if (n < 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	if (n % 2 == 0)
+		return (0);
+	return (_is_prime(n, 3));
 }
