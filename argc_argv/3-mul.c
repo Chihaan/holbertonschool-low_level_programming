deb3ac2a3ee2ba9e48f4e5d523604b9a7b4d25d3
Vadim G. <gavet.vadim@gmail.com>
2025-11-05 16:29:17 +0100

    Added file 3 of project 2

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 1;
+
+	if (argc != 3)
+	{
+		printf ("Error\n");
+		return (1);
+	}	
+
+	for (i = 1; i < argc; i++)
+		sum *= atoi(argv[i]);
+
+	printf("%d\n", sum);
+	return (0);
+}
