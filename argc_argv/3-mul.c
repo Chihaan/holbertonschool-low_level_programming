79872cceb1c18812748fd03797567f44ee7e7bd7
Vadim G. <gavet.vadim@gmail.com>
2025-11-05 16:32:15 +0100

    Fixed file 3 of project 2

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -16,10 +16,11 @@ int main(int argc, char *argv[])
 		printf ("Error\n");
 		return (1);
 	}	
-
-	for (i = 1; i < argc; i++)
+	else
+	{
+		for (i = 1; i < argc; i++)
 		sum *= atoi(argv[i]);
-
+	}
 	printf("%d\n", sum);
 	return (0);
 }
