ee89d1d6acc44b63e0dcb2523e44e814db0ad352
Vadim G. <gavet.vadim@gmail.com>
2025-11-05 10:56:30 +0100

    Fixed file 0 of project 3

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: argument vector
  * Return: Always 0 (Success)
  */
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
