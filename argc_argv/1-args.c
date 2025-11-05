37469183162931d1a66614c7787266979fc319b2
Vadim G. <gavet.vadim@gmail.com>
2025-11-05 11:05:11 +0100

    Added file 1 of project 2

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
