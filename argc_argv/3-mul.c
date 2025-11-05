d74aef761a3e37cd6774b0312810d947c5c279a9
Vadim G. <gavet.vadim@gmail.com>
2025-11-05 16:39:34 +0100

    Try fix in file 3 of project 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,15 +11,15 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 1;
 
-	if (argc < 2)
+	if (argc != 3)
 	{
-		printf ("Error\n");
+		printf("Error\n");
 		return (1);
-	}	
+	}
 	else if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
-		sum *= atoi(argv[i]);
+			sum *= atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
