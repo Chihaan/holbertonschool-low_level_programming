dd4a91766b4798e5a7764e0a8fdfc77b035eb615
Vadim G. <gavet.vadim@gmail.com>
2025-11-12 15:38:05 +0100

    Try fix in file 3

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -34,10 +34,10 @@ char *str_concat(char *s1, char *s2)
 	if (!ptr)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i]; i++)
 		ptr[i] = s1[i];
 
-	for (j = 0; s2[j] != '\0'; j++)
+	for (j = 0; s2[j]; j++)
 		ptr[i + j] = s2[j];
 		
 	ptr[i + j] = '\0';
