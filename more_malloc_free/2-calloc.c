e99d20ee6b7b9de38162a094ff3e246a22e3731f
Vadim G. <gavet.vadim@gmail.com>
2025-11-12 17:40:29 +0100

    Added file 2 of project 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,4 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *ptr;
+	unsigned int total = nmemb * size, i;
+
+
+	if (!size || !nmemb)
+		return (NULL);
 	
+	if (total / nmemb != size)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+		
+	for (i = 0; i < total; i++)
+		ptr[i] = 0;
+
+	return (ptr);
 }
