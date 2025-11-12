5b98a8c89e4e360909d6b223409eccdc86667563
Vadim G. <gavet.vadim@gmail.com>
2025-11-12 17:13:38 +0100

    added file 1 of project 2

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - alloue de la mémoire et quitte si échec
+ * @b: taille en octets à allouer
+ * Return: pointeur vers la mémoire allouée
+ */
 void *malloc_checked(unsigned int b)
 {
+	void *ptr = malloc(b);
+	if (!ptr)
+		exit (98);
 	
+	return (ptr);
 }
--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,58 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strlen - calcule la longueur d'une chaîne de caractères
+ * @s: chaîne dont on veut connaître la longueur
+ * Return: longueur de la chaîne
+ */
+int _strlen(char *s)
+{
+	unsigned int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+#include <stdlib.h>
+#include "main.h"
+/**
+ * string_nconcat - concatène s1 avec les n premiers caractères de s2
+ * @s1: première chaîne
+ * @s2: seconde chaîne
+ * @n: nombre max de caractères de s2 à concaténer
+ * Return: nouvelle chaîne concaténée, ou NULL si erreur
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	
+	unsigned int i, j, len1, len2, o;
+	char *sncon, *tmp;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	if (n >= len2)
+		o = len2;
+	else
+		o = n;
+
+	sncon = malloc((len1 + o + 1) * sizeof(char));
+	if (!sncon)
+		return (NULL);
+
+	tmp = sncon;
+
+	for (i = 0; i < len1; i++)
+		sncon[i] = s1[i];
+	for (j = 0; j < o; j++)
+		sncon[i + j] = s2[j];
+
+	sncon[i + j] = '\0';
+
+	return (tmp);
 }
