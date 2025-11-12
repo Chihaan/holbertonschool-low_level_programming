e241963b64efc53913cbc000d41afc3023d01e13
Vadim G. <gavet.vadim@gmail.com>
2025-11-12 17:45:27 +0100

    Try fix in file 2 of project 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include "main.h"
-
+/**
+ * _calloc - alloue de la mémoire pour un tableau et initialise à 0
+ * @nmemb: nombre d'éléments
+ * @size: taille de chaque élément
+ * Return: pointeur vers la mémoire allouée, ou NULL si erreur
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
@@ -9,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (!size || !nmemb)
 		return (NULL);
-	
+
 	if (total / nmemb != size)
 		return (NULL);
 
