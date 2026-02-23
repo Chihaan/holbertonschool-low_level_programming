d4b39b5096c7586506699ba1cb8d9fe2795f8790
Chihaan <you@example.com>
2026-02-23 11:08:37 +0100

    Added file 3-

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l = 'a', u = 'A';
+
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	while (u <= 'Z')
+	{
+		putchar(u);
+		u++;
+	}
+	putchar('\n');
+	return (0);
+}
