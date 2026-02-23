d44b6e35ea4ed6240195ebdc196435ac7ee02f82
Chihaan <you@example.com>
2026-02-23 14:01:14 +0100

    Added file 5-

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of the number if positive or negative
+ * @n : Number we are checking
+ * Return: Nothing
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('\n');
+}
