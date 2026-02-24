79fc0d846cbbdc7335d368b249f55b220d9411fc
Chihaan <you@example.com>
2026-02-24 14:29:22 +0100

    Added file 2-

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Multiplies two integers
+ * @a : The int a used
+ * @b : The int b used
+ * Return: sum of a * b
+ */
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+
+	return (sum);
+}
