27b7b587b47200d844aef862b31f13e822332a7c
Chihaan <you@example.com>
2026-02-24 14:24:07 +0100

    added file 0-

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - Checks if a charachter is uppercase
+ * @c : The character tested
+ * Return: Nothing
+ */
+int _isupper(int c)
+{
+		if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
