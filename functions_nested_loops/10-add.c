3347b7160b909932db4e05dcd9cbb96f12808aca
Chihaan <you@example.com>
2026-02-24 09:25:02 +0100

    Added file 10-

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Add two numbers
+ *
+ * @a : Number to add
+ * @b : Number to add
+ * Return: the sum of the addition
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
