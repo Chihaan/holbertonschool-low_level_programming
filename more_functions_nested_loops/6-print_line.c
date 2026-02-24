f1a597d13506c96dfcac07041163508f10a1cfe9
Chihaan <you@example.com>
2026-02-24 15:24:06 +0100

    Added file 6-

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line- Prints a libe of n length
+ *
+ * Return: Nothing
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
