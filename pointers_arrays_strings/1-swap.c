275a49d65b2d7e225a3d15361748e1320cde59ab
Chihaan <you@example.com>
2026-03-02 15:00:33 +0100

    Added S5P1F2

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,6 +1,15 @@
 #include "main.h"
 /**
- * main - check the code 
- *
- * Return: Always 0.
+ * swap_int - swap the value of two integers
+ * @a : value a
+ * @b : Value b
+ * Return: nothing
  */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
+
