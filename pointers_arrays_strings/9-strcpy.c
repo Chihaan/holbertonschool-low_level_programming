d7774cd3bbe94a56f06f16276ff346d9df9ee18e
Chihaan <you@example.com>
2026-03-02 19:06:16 +0100

    Added S5P1F9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,19 @@
 #include "main.h"
 /**
- * main - check the code 
- *
- * Return: Always 0.
+ * *_strcpy - Copy a string from src to dest
+ * @dest : Destination adress
+ * @src : Source adress
+ * Return: Pointer to dest
  */
+char *_strcpy(char *dest, char *src)
+{
+	char *odest = dest, *osrc = src;
+
+	while (*src <= "\0")
+	{
+		*odest = *osrc;
+		odest++;
+		osrc++;
+	}
+	return (dest);
+}
