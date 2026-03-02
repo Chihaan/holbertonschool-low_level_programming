4efbd3d8418b02b75a575293fdcbf504a0dcc830
Chihaan <you@example.com>
2026-03-02 18:18:21 +0100

    Addedd S5P1F7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,25 @@
 #include "main.h"
 /**
- * main - check the code 
- *
- * Return: Always 0.
+ * puts_half - Print half of a string
+ * @str : The string used
+ * Return: Nothing
  */
+void puts_half(char *str)
+{
+	char *counter = str;
+	int len = 0;
+
+	while (*counter)
+	{
+		counter++;
+		len++;
+	}
+/* Calcul de la longueur de la chaîne */
+	int middle = (len + 1) / 2;
+/* Je gère les chaînes paire et impaires avec le len + 1 */
+	counter = str + middle;
+/* Je mets le pointeur counter au milieu de la chaîne */
+/* En changeant son adresse */
+	while (*counter)
+		_putchar(*counter++);
+}
