b7951b919d6d60af9f6b4572c6f9abc94f19dab4
Chihaan <you@example.com>
2026-03-02 18:25:57 +0100

    Addedd S5P1F8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,15 @@
 #include "main.h"
 /**
- * main - check the code 
- *
+ * print_array - Print n elements of an array.
+ * @a : The array
+ * @n : The number of elements wanted
  * Return: Always 0.
  */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i <= n)
+		_putchar(*a++);
+	_putchar('\n');
+}
