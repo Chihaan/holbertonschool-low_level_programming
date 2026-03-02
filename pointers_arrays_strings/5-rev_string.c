5834634068b6f8e6d10ecb00a403545a6d9f0fce
Chihaan <you@example.com>
2026-03-02 16:12:45 +0100

    Fixed Betty in S5P1F6

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,23 +6,21 @@
  */
 void rev_string(char *s)
 {
-	char *end =s, tmp; 
+	char *end = s, tmp;
 /*Je crée un pointeur pour l'emmener a la fin et un tmp*/
 	while (*end)
 		end++;
-	end--;	
+	end--;
 /*Je recule d'un car end est sur '\0'*/
 	while (s < end)
 	{
 		tmp = *s;
+/*On met s dans tmp pour ne pas perdre sa valeur*/
 		*s = *end;
 		*end = tmp;
+/*On met end dans s et tmp(s) dans end*/
 		s++;
 		end--;
+/*On avance le pointeur du début et recule celui de la fin*/
 	}
-/**
- * On met s dans tmp pour ne pas perdre sa valeur
- * On met end dans s et tmp(s) dans end
- * On avance le pointeur du début et recule celui de la fi
- */
 }
