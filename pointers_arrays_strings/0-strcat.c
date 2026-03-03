0ad7aad9ee58dec600e07df3cebd1dbfba8936c9
Chihaan <you@example.com>
2026-03-03 09:42:31 +0100

    Added S5P2F0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,22 @@
 #include "main.h"
 /**
- * main - check the code 
- *
- * Return: Always 0.
+ * *_strcat - Overwrite the string des from src
+ * @dest : The destination string
+ * @src : The source string
+ * Return: Nothing
  */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+}
