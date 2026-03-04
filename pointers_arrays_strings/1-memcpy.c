423d3d6837699ec4f04050a0184a2e426eb55263
Chihaan <you@example.com>
2026-03-04 15:55:28 +0100

    Added S5P3F1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,21 @@
 #include "main.h"
 /**
- * main - check the code 
- *
+ * *_memcpy - Copies memory area
+ * @dest : Area destination
+ * @src : Area Source
+ * @n : Number of copies
  * Return: Always 0.
  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *ptr = dest;
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (ptr);
+}
