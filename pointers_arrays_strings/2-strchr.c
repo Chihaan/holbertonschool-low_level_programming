e5fa26ec04bffa2ae127ee8afcdad0baf3327d64
Chihaan <you@example.com>
2026-03-04 15:59:11 +0100

    Added S5P3F2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,17 @@
 #include "main.h"
 /**
- * main - check the code 
- *
- * Return: Always 0.
+ * *_strchr - Locates a character in a string
+ * @s : The string
+ * @c : The char we look for
+ * Return: s if found or 0
  */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
