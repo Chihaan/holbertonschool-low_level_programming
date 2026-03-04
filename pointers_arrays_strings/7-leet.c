0df38bace43ad9e0e0d1e2bfc2a58dcaae990ad1
Chihaan <you@example.com>
2026-03-04 13:12:59 +0100

    Added S5P2F7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,6 +1,27 @@
 #include "main.h"
 /**
- * main - check the code 
- *
+ * *leet - Encode a string into 1337
+ * @s : the string
  * Return: Always 0.
  */
+char *leet(char *s)
+{
+	char *letter = "aAeEoOtTlL", *leet = "4433007711", *ptr = s;
+	int i = 0;
+
+	while (*s)
+	{
+		while (letter[i] && leet[i])
+/* Tant que la chaine n'est pas finie */
+		{
+			if (*s == letter[i])
+				*s = leet[i];
+/* Si l'un des char est dans ma liste je le remplace */
+			i++;
+		}
+		s++;
+		i = 0;
+/* Je réinitialise i a 0 */
+	}
+	return (ptr);
+}
