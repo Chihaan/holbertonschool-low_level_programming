7a61d33b01100f1b2a9e6b4059b6f1d47a319cab
Chihaan <you@example.com>
2026-03-04 10:01:34 +0100

    Added S5P2F2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * *_strncpy - Copy a string 
+ * *_strncpy - Copy a string
  * @dest : Destination string
  * @src : Source string
- * @n : 
+ * @n : number of chars
  * Return: Always 0.`
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int count = 0;
 	char *ptr = dest;
-	
+
 	while (*src != '\0' && count < n)
-	{	
+	{
 		*dest = *src;
 		count++;
 		dest++;
@@ -23,6 +23,6 @@ char *_strncpy(char *dest, char *src, int n)
 		*dest = '\0';
 		dest++;
 		count++;
-	}	
-	return (ptr);			
+	}
+	return (ptr);
 }
