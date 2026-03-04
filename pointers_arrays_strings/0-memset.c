6dd627d3be2fbe094ab8e92253d6d20183fab5d9
Chihaan <you@example.com>
2026-03-04 14:16:41 +0100

    Added S5P3F0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,21 @@
 #include "main.h"
 /**
- * main - check the code 
- *
+ * *_memset - Fills memory with a constant byte
+ * @s : Pointer to a string
+ * @b : A string
+ * @n : Number of bytes to replace with b
  * Return: Always 0.
  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	char *ptr = s;
+
+	while (i < n)
+	{
+		*s = b;
+		i++;
+		s++;
+	}
+	return (ptr);
+}
