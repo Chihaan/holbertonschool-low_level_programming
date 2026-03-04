c8c1b58dd50cc700ba259b0aa1f7d61a550170ab
Chihaan <you@example.com>
2026-03-04 12:31:30 +0100

    Added S5P2F6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,31 @@
 #include "main.h"
 /**
- * main - check the code 
- *
- * Return: Always 0.
+ * *cap_string - Capitalize all words of a string
+ * @s : The string
+ * Return: A pointer to the string
  */
+char *cap_string(char *s)
+{
+	char *ptr = s, *sep = " '\t\n,;.!?\"(){}", *debut = sep;
+	int find = 0;
+
+	while (*s)
+	{
+		if (*s >= 'A' && *s <= 'Z' && find == 0)
+			find = 1;
+		if (*s >= 'a' && *s <= 'z' && find == 0)
+		{
+			*s -= 32;
+			find = 1;
+		}
+		while (*sep)
+		{
+			if (*s == *sep)
+				find = 0;
+			sep++;
+		}
+		sep = debut;
+		s++;
+	}
+	return (ptr);
+}
