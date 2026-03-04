a59ef1edf853203b04e9ef218f4515331b2b8d59
Chihaan <you@example.com>
2026-03-04 11:49:17 +0100

    Added S5P2F5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,21 @@
 #include "main.h"
 /**
- * main - check the code 
- *
- * Return: Always 0.
+ * *string_toupper - Change all lowercase to uppercase
+ * @s : The string
+ * Return: A pointer to the start of the string
  */
+char *string_toupper(char *s)
+{
+	char *start = s;
+
+	while (*s)
+/* Tant que je suis pas a la fin de ma chaine*/
+	{
+/* Si le char que je lis est une minuscule */
+		if (*s >= 'a' && *s <= 'z')
+/* Passe le en majuscule */
+			*s -= 32;
+		s++;
+	}
+	return (start);
+}
