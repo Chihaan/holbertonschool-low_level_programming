7e30e774df86fc1a8dbce45be1220aa3890a5df4
Chihaan <you@example.com>
2026-03-04 11:34:16 +0100

    Added S5P2F4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,26 @@
 #include "main.h"
 /**
- * main - check the code 
- *
+ * reverse_array - Reverse the content of an array
+ * @a : The array
+ * @n : The number of elements
  * Return: Always 0.
  */
+void reverse_array(int *a, int n)
+{
+	int i = 0, tmp = 0, *end = a;
+
+	while (i < n - 1)
+	{
+		i++;
+		end++;
+	}
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = *a;
+		*a = *end;
+		*end = tmp;
+		a++;
+		end--;
+	}
+}
