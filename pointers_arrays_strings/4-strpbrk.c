500babac1531b4544d8a48000348dafd2a4bc52a
Chihaan <you@example.com>
2026-03-05 09:52:46 +0100

    Added S5P3F4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,24 @@
 #include "main.h"
 /**
- * main - check the code 
- *
- * Return: Always 0.
+ * *_strpbrk - Searches a string for any of a set of bytes
+ * @s : The string
+ * @accept : The test string
+ * Return: A pointer to the first occurence
  */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a = accept;
+
+	while (*s)
+	{
+		while (*accept)
+		{
+			if (*s == *accept)
+				return (s);
+			accept++;
+		}
+		s++;
+		accept = a;
+	}
+	return (0);
+}
