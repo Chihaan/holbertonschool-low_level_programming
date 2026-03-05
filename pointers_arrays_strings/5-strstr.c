559aea34f94ab82da16a5c99bc7508bc1d11c663
Chihaan <you@example.com>
2026-03-05 11:16:19 +0100

    Added S5P3F4

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,36 @@
 #include "main.h"
 /**
- * main - check the code 
- *
- * Return: Always 0.
+ * *_strstr - Finds the first occurrence of the substring
+ * @haystack : The source file 
+ * @needle : The substring
+ * Return: A pointer to the first occurence
  */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h = haystack, *n = needle;
+	int count = 0, needle_len = 0;
+
+	while (*needle)
+		needle_len += 1;
+
+	while (*haystack)
+	{
+		if (*haystack == *needle)
+		{
+			h = haystack;
+			while (*needle)
+			{
+				if (*needle != *haystack)
+					needle = h;
+					break;
+				needle++;
+				haystack++;
+				count += 1;
+				if (count == needle_len)
+					return (h);
+			}
+			haystack++;
+		}
+	}
+	return (0);
+}
