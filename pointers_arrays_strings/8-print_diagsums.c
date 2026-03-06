710f6a4fe71c3c1c24abf5dacefd05ded91d948e
Chihaan <you@example.com>
2026-03-06 09:19:26 +0100

    Added S5P3F8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -18,6 +18,5 @@ void print_diagsums(int *a, int size)
 			sum2 += a[i * size + (size - 1 - i)];
 /* Anti diagonale */
 	}
-	printf("%d, ", sum);
-	printf("%d\n", sum2);
+	printf("%d, %d\n ", sum, sum2);
 }
