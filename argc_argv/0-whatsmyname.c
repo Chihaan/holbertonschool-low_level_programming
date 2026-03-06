4c6868ba40fe24f291a974ead83ec52b8afc6b1c
Chihaan <you@example.com>
2026-03-06 10:06:19 +0100

    Added S5P4F0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define UNUSED(argc) (int)(argc)
+/**
+ * main - Entry of program
+ * @argc : Argument count
+ * @argv : Pointer to array of strings
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	UNUSED(argc);
+	printf("%s\n", *argv);
+}
