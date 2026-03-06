7b359fdfe5b31fb5120f9230ba2ef3a63677147b
Chihaan <you@example.com>
2026-03-06 11:20:22 +0100

    Modified file

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - Print the sum of the diagonals 
+ * print_diagsums - Print the sum of the diagonals
  * @a : the array
  * @size : the size of the array
- * Return: Always 0.
+ * Return: No return
  */
 void print_diagsums(int *a, int size)
 {
-	int sum = 0; 
+	int sum = 0;
 	int sum2 = 0;
 	int i = 0;
 
-	while (i < size)
+	for (i = 0; i < size; i++)
 /* Je boucle dans le tableau 1d */
 	{
 		sum += a[i * (size + 1)];
 /* Premiere diagonale */
 		sum2 += a[i * size + (size - 1 - i)];
 /* Anti diagonale */
-		i++;
 	}
 	printf("%d, %d\n", sum, sum2);
 }
