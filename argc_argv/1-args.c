e6b1b147d5c5d762a64f722bd9b55359c5de192f
Chihaan <you@example.com>
2026-03-06 10:26:08 +0100

    Added S5P4F1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry of program
+ * @argc : Argument count
+ * @argv : Pointer to array of strings
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
