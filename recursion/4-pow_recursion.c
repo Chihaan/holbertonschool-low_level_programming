a9c774eeaa09278e7add89518638eec4b96658b4
Chihaan <you@example.com>
2026-03-14 18:12:37 +0100

    Added file 4 recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - Value of x raised by y
+ * @x: The first number
+ * @y: The power
+ * Return: Result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * (_pow_recursion(x, y - 1)));
+}
