b7f8a54e5d8a0bd93e4da37caf0b8db4f12b2067
Chihaan <you@example.com>
2026-03-16 10:53:50 +0100

    Added file 1 of variadic functions

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,12 +3,24 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- * print_name - Prints the name as is
- * @name : The name
- * @f : The functions
- * Return: Nothing
+ * print_numbers - Prints numbers
+ * @separator: The string separator
+ * @n: Number of int
+ * Return: The sum of le x arguments
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list ap;
+	unsigned int i = 0;
 
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+			printf("%s", separator);
+		printf("%d", va_arg(ap, int));
+	}
+	va_end(ap);
+	printf("\n");
 }
