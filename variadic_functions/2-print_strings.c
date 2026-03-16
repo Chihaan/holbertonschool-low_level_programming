bddf807736d7b67f63751f1474b218624b5ebecc
Chihaan <you@example.com>
2026-03-16 11:08:52 +0100

    Added file 1 of variadic functions

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -3,12 +3,29 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- * print_name - Prints the name as is
- * @name : The name
- * @f : The functions
- * Return: Nothing
+ * print_strings - Prints strings
+ * @separator: The string separator
+ * @n: Number of int
+ * Return: The sum of le x arguments
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i = 0;
+	va_list ap;
+	char *str;
 
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0 && separator != NULL)
+			printf("%s", separator);
+		str = va_arg(ap, char *);
+		if (str == NULL)
+    			printf("(nil)");
+		else
+    			printf("%s", str);
+	}
+	va_end(ap);
+	printf("\n");
 }
