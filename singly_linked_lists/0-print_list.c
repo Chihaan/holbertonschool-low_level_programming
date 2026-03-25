8b411965672aa10d8a4c208d9d5a1a7db502259d
Chihaan <you@example.com>
2026-03-25 11:11:26 +0100

    Added file 0 of project singly linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		nodes += 1;
+	}
+	return (nodes);
+}
