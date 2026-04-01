82bd9554f2bedb15e5a7477b0f9be30c220e04c7
Chihaan <you@example.com>
2026-04-01 21:02:00 +0200

    Added file : 4rd exercice hash tables

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -13,6 +13,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *node;
 
+	if (!ht || !key || !*key)
+		return (NULL);
+
 	idx = key_index((unsigned char *)key, ht->size);
 	node = ht->array[idx];
 
