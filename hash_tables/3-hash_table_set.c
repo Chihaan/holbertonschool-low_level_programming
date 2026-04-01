f4d88a4e9941d21d9a1c5f45c95204e5a938c894
Chihaan <you@example.com>
2026-04-01 18:10:35 +0200

    Added file : 3rd exercice hash tables

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,6 +3,35 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
+ * *create_node - Create a node for hashtable
+ * @key: the key
+ * @value: the value
+ * Return: Pointer to the node
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (NULL);
+	new->key = strdup(key);
+	if (!new->key)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->value = strdup(value);
+	if (!new->value)
+	{
+		free(new->key);
+		free(new);
+		return (NULL);
+	}
+	new->next = NULL;
+	return (new);
+}
+/**
  * hash_table_set - Adds an element in the hashtable
  * @ht: The table
  * @key: the key
@@ -23,8 +52,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	while (node)
 	{
-		/* Si la clé est la même on remplace la value (UPDATE) */
-		if (strcmp(node->key, key) == 0) 
+		/* Si la clé est la même on remplace la value */
+		if (strcmp(node->key, key) == 0)
 		{
 			free(node->value);
 			node->value = strdup(value);
@@ -32,23 +61,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		node = node->next;
 	}
-	new = malloc(sizeof(hash_node_t));
+	new = create_node(key, value);
 	if (!new)
 		return (0);
-
-	new->key = strdup(key);
-	if (!new->key)
-	{
-		free (new);
-		return (0);
-	}
-	new->value = strdup(value);
-	if (!new->value)
-	{
-		free(new->key);
-		free(new);
-		return (0);
-	}
 	new->next = ht->array[idx];
 	ht->array[idx] = new;
 
