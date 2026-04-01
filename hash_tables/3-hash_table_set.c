4e11b0f7bcbe9bd90babf7f1a974b1a4c78933c0
Chihaan <you@example.com>
2026-04-01 17:59:43 +0200

    Added file : 3rd exercice hash tables

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -4,12 +4,53 @@
 #include "hash_tables.h"
 /**
  * hash_table_set - Adds an element in the hashtable
- * @ht:
- * @key:
- * @value: 
- * Return: size_t
+ * @ht: The table
+ * @key: the key
+ * @value: the value
+ * Return: 1 if ok 0 if failed
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	
+	unsigned long int idx;
+	hash_node_t *node, *new;
+
+	/* Verification de ht, key & * key */
+	if (!ht || !key || !*key)
+		return (0);
+
+	idx = key_index(key, ht->size);
+	node = ht->array[idx];
+
+	while (node)
+	{
+		/* Si la clé est la même on remplace la value (UPDATE) */
+		if (strcmp(node->key, key) == 0) 
+		{
+			free(node->value);
+			node->value = strdup(value);
+			return (1);
+		}
+		node = node->next;
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+
+	new->key = strdup(key);
+	if (!new->key)
+	{
+		free (new);
+		return (0);
+	}
+	new->value = strdup(value);
+	if (!new->value)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+
+	return (1);
 }
