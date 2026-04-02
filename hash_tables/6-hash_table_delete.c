8419c05e061455d873f432deaed375908deb782c
Chihaan <you@example.com>
2026-04-02 15:26:21 +0200

    Added file : 6th exercice of hash table project

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -3,8 +3,39 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
- * hash_table_create - Prints all the elements of the list
- * @size: pointer to the head of the list
- * Return: size_t
+ * free_list - Frees a list
+ * @node: pointer to the first node of a list
+ * Return: void
+ */
+void free_list(hash_node_t *node)
+{
+	hash_node_t *tmp;
+
+	while (node)
+	{
+		tmp = node->next;
+		free(node->key);
+		free(node->value);
+		free(node);
+		node = tmp;
+	}
+}
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table
+ * Return: void
  */
 
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *del;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		del = ht->array[i];
+		free_list(del);
+	}
+	free(ht->array);
+	free(ht);
+}
