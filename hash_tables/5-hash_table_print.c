7541b266b67631d8a5f05aa61c9f656bdb996c87
Chihaan <you@example.com>
2026-04-02 15:07:27 +0200

    Added file : 5th exercie of project hash table

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -3,7 +3,31 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
- * hash_table_create - Prints all the elements of the list
- * @size: pointer to the head of the list
+ * hash_table_print - Prints a hash table
+ * @ht: pointer to the head of the list
  * Return: size_t
  */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long i = 0, size;
+	int first = 1;
+
+	printf("{");
+	size = ht->size;
+	node = ht->array[i];
+
+	for (i = 0; i < size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (!first)
+				printf(", ");
+			first = 0;
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
